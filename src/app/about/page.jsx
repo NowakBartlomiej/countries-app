631c2a34cd48b1612338b6b520ab0937a17d6900
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-08-21 10:05:06 +0200

    Implemented simple routing

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/header"
+
+export const metadata = {
+    title: 'About'
+}
+
+const page = () => {
+  return (
+    <div>
+        <Header />
+        <h1>About Page</h1>
+    </div>
+  )
+}
+
+export default page
