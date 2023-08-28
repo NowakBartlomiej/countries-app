9d7dd694d85508d3a2e58da16f715c522a04d713
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-08-28 11:45:30 +0200

    I change h1 tag to p tag

--- src/app/countries/[slug]/page.jsx
+++ src/app/countries/[slug]/page.jsx
@@ -19,7 +19,7 @@ const Page = ({params}) => {
   
   return (
     <section>
-        <h1><span className={styles.span}>Country: </span> {params.slug}</h1>
+        <p className={styles.text}><span className={styles.span}>Country: </span> {params.slug}</p>
 
         <div onClick={() => router.back()} className={styles.button}>Go back</div>
     </section>
