96bdebc5e4f56f7bcf8b9088be22ce148826d534
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-09-04 11:10:13 +0200

    add sematic element nav with styles to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,10 +9,12 @@ const Header = () => {
   const currentRoute = usePathname();
   
   return (
-    <header className={styles.navigation}>
+    <header>
+        <nav className={styles.navigation}>
           <Link className={currentRoute === '/' ? styles.activeLink : styles.link} href="/">Home</Link> 
           <Link className={currentRoute === '/about' ? styles.activeLink : styles.link} href="/about">About</Link>
           <Link className={currentRoute === '/countries' ? styles.activeLink : styles.link} href="/countries">Countries</Link>
+        </nav>
     </header>
   );
 }
