bf01452139c969190b840919e7e910be4682be6c
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-09-04 11:06:13 +0200

    import global value, and use it in about page

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const path = require('path')
 const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
+        prependData: `@import "~@styles/variables.scss";`,
       },
 }
 
