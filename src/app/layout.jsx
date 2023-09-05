a638960c4bc6fcf8c1bdffee93fd90894876feba
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-09-05 09:58:20 +0200

    Create react query provider

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,19 +2,22 @@ import './globals.scss'
 import { Inter } from 'next/font/google'
 
 import Header from '@/components/header/header';
+import ReactQueryProvider from './ReactQueryProvider';
 
 const inter = Inter({subsets: ['latin']})
 
 const Layout = ({ children }) => {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        <main>
-          {children}
-        </main>
-      </body>
-    </html>
+    <ReactQueryProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          <main>
+            {children}
+          </main>
+        </body>
+      </html>
+    </ReactQueryProvider>
   )
 }
 
