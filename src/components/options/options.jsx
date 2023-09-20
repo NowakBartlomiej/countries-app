d35141fc3b66b2333bf5d4e9ee8af203bd11ccff
Bartlomiej <16bartlomiejnowak@gmail.com>
2023-09-20 13:00:56 +0200

    format options file

--- src/components/options/options.jsx
+++ src/components/options/options.jsx
@@ -1,23 +1,19 @@
 import styles from './style.module.scss'
-import Image from 'next/image'
-import {continents} from '@utils/continents'
+import { continents } from '@utils/continents'
 
 const Options = () => {
   return (
     <section className={styles.options}>
-        <select className={styles.select} name="" id="">
-            <option value="">Sort by name A-Z</option>
-            <option value="">Sort by name Z-A</option>
-        </select>
-        <div className={styles.continents}>
-            {continents.map((continent) => (
-              <p key={continent.name} className={continent.isActive ? styles.activeContinent : styles.continent}>{continent.name}</p>
-            ))}
-        </div>
-        
-    
-        <input type='search' className={styles.input} placeholder='search...'/>
-        
+      <select className={styles.select} name="" id="">
+        <option value="">Sort by name A-Z</option>
+        <option value="">Sort by name Z-A</option>
+      </select>
+      <div className={styles.continents}>
+        {continents.map((continent) => (
+          <p key={continent.name} className={continent.isActive ? styles.activeContinent : styles.continent}>{continent.name}</p>
+        ))}
+      </div>
+      <input type='search' className={styles.input} placeholder='search...' />
     </section>
   )
 }
